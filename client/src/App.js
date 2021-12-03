bde0cf1fa13973b3d8e14032b0e54b7cca97c9ad
Craig Marshall <dev.craigmarshall@gmail.com>
2021-12-03 11:45:57 +0000

    added a scroll-to link

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,50 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Planet Earth! How you doing?</h1>
+      <a href="#quiz">Go To Quiz</a>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <div id="quiz">
+        <h1>Quiz</h1>
+        <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      </div>
     </div>
   );
 }
