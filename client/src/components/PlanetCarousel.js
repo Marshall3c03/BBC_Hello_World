9c267102c2265925e5ab0e837d5ef5fff2095e8f
Craig Marshall <dev.craigmarshall@gmail.com>
2021-12-04 13:04:34 +0000

    Managed to get a single carousel item, which changes on arrow click

--- client/src/components/PlanetCarousel.js
+++ client/src/components/PlanetCarousel.js
@@ -1,22 +1,40 @@
 import React, {useState} from 'react';
 import { PlanetSliderData } from './PlanetSliderData';
 
-const PlanetCarousel = () => {
+const PlanetCarousel = ({ slides }) => {
 
     const [current, setCurrent] = useState(3)
-    // const length = slides.length
+    const length = slides.length
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    if (!Array.isArray(slides) || slides.length <= 0){
+        return null;
+    }
+
+    console.log(current)
 
     return(
-        <div>
+        <section>
+            <img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/39040/preview.png" width="35px"  onClick={prevSlide}/>
+            
            {PlanetSliderData.map((slide, index) => {
                return(
                    <div>
-                        <img src={slide.image} alt="planet image" width="175px"/>
+                       {index === current && (
+                       <img src={slide.image} alt="planet image" width="175px" height="175px"/>)}
+                        {/* <img src={slide.image} alt="planet image" width="175px"/> */}
                    </div>
                )
            })}
-        </div>
+           <img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/39041/preview.png" width="35px" onClick={nextSlide}/>
+        </section>
     )
 }
 
--- client/src/containers/PlanetInfo.js
+++ client/src/containers/PlanetInfo.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PlanetCarousel from '../components/PlanetCarousel';
 import PlanetFactFile from '../components/PlanetFactFile';
 import PlanetLayout from '../components/PlanetLayout';
+import { PlanetSliderData } from '../components/PlanetSliderData';
 
 const PlanetInfo = () => {
     return(
         <div>
             <PlanetLayout/>
-            <PlanetCarousel/>
+            <PlanetCarousel slides={PlanetSliderData}/>
             <PlanetFactFile/>
         </div>
     )
