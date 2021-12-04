56073cf6e94163c67a1ac9a2f4b09f4733746248
Anita Safar <ancsa0416@gmail.com>
2021-12-04 12:49:55 +0000

    connect method added, arguments passed in

--- server/server.js
+++ server/server.js
@@ -7,4 +7,6 @@ app.use(cors());
 const createRouter = require('./helpers/create_router');
 const MongoClient = require('mongodb').MongoClient;
 
+MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology:true})
+
 app.listen(5000);
