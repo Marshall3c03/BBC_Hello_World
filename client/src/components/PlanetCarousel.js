5c456573ef7375a9b29cb275fec5b8304fd91619
Craig Marshall <dev.craigmarshall@gmail.com>
2021-12-04 11:16:31 +0000

    Added more content to front end, added a static folder for including pictures

--- client/src/components/PlanetCarousel.js
+++ client/src/components/PlanetCarousel.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Earth from '../static/EarthAvatar.jpeg'
 
 const PlanetCarousel = () => {
     return(
         <div>
-            <p>Carousel</p>
+            <img src={Earth} width="175px"/>
+            <ul>
+                <li>
+                    <p>Name: Earth</p>
+                </li>
+                <li>
+                    <p>Diameter: 12742km</p>
+                </li>
+            </ul>
+            <button>Select Planet</button>
         </div>
     )
 }
