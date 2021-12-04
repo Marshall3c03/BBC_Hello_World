a4addd050376ed82c115c77181646c51a417e1b8
Sam <samspencer2202@outlook.com>
2021-12-04 18:46:54 +0000

    chart displaying

--- client/src/components/PlanetChart.js
+++ client/src/components/PlanetChart.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Bar } from 'react-chartjs-2';
 
 const PlanetChart = () => {
     return(
         <div>
             <h1>CHARTS</h1>
             <p> ######## Chart goes here ######## </p>
+            <Bar
+                datasetIdKey='id'
+                data={{
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets:[
+                        {
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                        },
+                    ],
+                }}
+                // height={200}
+                // width={250}
+                // options={{
+                //     maintainAspectRatio: false,
+                // }}
+            />
         </div>
     )
 }
