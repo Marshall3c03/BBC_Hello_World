74565b477109544a4385ef58fc3782ecee819fe0
Anita Safar <ancsa0416@gmail.com>
2021-12-04 13:14:33 +0000

    find and toArray methods added to get back an array of all documents

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -4,7 +4,9 @@ const createRouter = function (collection) {
     const router = express.Router();  
 
     router.get('/', (req, res) => { 
-       
+        collection
+        .find()
+        .toArray()
     });
 
    return router;
