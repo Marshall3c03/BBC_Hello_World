fae4c062da454612db5e2d996b4dc2fd9f1b9a19
Anita Safar <ancsa0416@gmail.com>
2021-12-04 12:56:30 +0000

    get client object/catch error

--- server/server.js
+++ server/server.js
@@ -8,5 +8,7 @@ const createRouter = require('./helpers/create_router');
 const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology:true})
+    .then((client) => {})
+    .catch(console.error);
 
 app.listen(5000);
