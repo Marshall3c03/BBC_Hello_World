b3bd86bb67a8318b2e69f397f05261a7ea8bbd90
Sam <samspencer2202@outlook.com>
2021-12-05 04:56:27 +0000

    bar+polar chart driven by db

--- client/src/components/PlanetChart.js
+++ client/src/components/PlanetChart.js
@@ -1,46 +1,82 @@
-import React, { useEffect, useState } from "react";
-import { Bar } from 'react-chartjs-2';
+import React from "react";
+import {Bar, PolarArea } from 'react-chartjs-2';
 
 const PlanetChart = ({ planetsList }) => {
 
-    const planetNames = []
+    const planetNamesBar = []
+    const planetNamesPolar = []
     const planetDistance = []
+    const planetDiameter = []
 
-    const planetsNames = planetsList.map((planet) => {
-        planetNames.push(planet.name)
+    const planetsNamesBar = planetsList.map((planet) => {
+        if (planet.name !== 'Earth'){
+            planetNamesBar.push(planet.name)
+        }
     })
-
+    const planetsNamesPolar = planetsList.map((planet) => {
+        if (planet.name !== 'Sun'){
+            planetNamesPolar.push(planet.name)
+        }
+    })
     const planetsDistances = planetsList.map((planet) => {
-        planetDistance.push(planet.distanceFromEarth)
+        if (planet.name !== 'Earth'){
+            planetDistance.push(planet.distanceFromEarth)
+        }
+    })
+    const planetsDiameters = planetsList.map((planet) => {
+        if (planet.name !== 'Sun'){
+            planetDiameter.push(planet.diameter)
+        }
     })
 
-    console.log(planetsList)
-    console.log(planetNames)
+    console.log(planetsList);
+    console.log(planetNamesBar);
 
     return(
         <div>
             <h1>CHARTS</h1>
-            <p> ######## Chart goes here ######## </p>
+            {/* <p> ######## Chart goes here ######## </p> */}
             <Bar
                 data={{
-                    labels: planetNames,
-                    datasets:[
-                        {
-                            label: 'Distance from Earth',
+                    labels: planetNamesBar,
+                    datasets:[{
+                            label: 'Distance from Earth (km)',
                             data: planetDistance,
                             backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
-                            ],
-                        },
-                    ],
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ]
+                    }]
+                }}
+                options = {{
+                    indexAxis: 'y' 
                 }}
                 height={1000}
                 width={2000}
+            />
+
+            <PolarArea
+                data={{
+                    labels: planetNamesPolar,
+                    datasets:[{
+                            label: 'Planet Diamete (km)',
+                            data: planetDiameter,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ]
+                    }]
+                }}
+                options = {{
+                }}
             />
         </div>
     )
