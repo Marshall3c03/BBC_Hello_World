ded7f1564510a0197dd97bb5ab0e51fb51901d6a
Anita Safar <ancsa0416@gmail.com>
2021-12-06 11:44:24 +0000

    solarsystem css added, imported

--- client/src/containers/SolarSystem.js
+++ client/src/containers/SolarSystem.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SolarInfo from "../components/SolarInfo";
+import "./SolarSystem.css"
 
 const SolarSystem = () => {
     return(
-        <div>
+        <div id="solarsystem">
             <h3>Solar System</h3>
             <SolarInfo/>
         </div>
