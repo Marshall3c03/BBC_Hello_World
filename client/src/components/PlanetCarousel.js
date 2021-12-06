2ebc437f0ae2261ebdaa39f63f81699dee4a57f6
Ross Napier <ross-napier@hotmail.com>
2021-12-06 15:13:11 +0000

    Images changed to local

--- client/src/components/PlanetCarousel.js
+++ client/src/components/PlanetCarousel.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Earth from '../static/Earth.png';
 
 const PlanetCarousel = ({ planets, slides }) => {
 
@@ -21,21 +22,28 @@ const PlanetCarousel = ({ planets, slides }) => {
     return(
         <section>
            {planets.map((planet, index) => {
+
+               const displayImage = (planet) => {
+                
+               }
+
                return(
+                   <>
                    <div>
                         {index === current && (
+                            
                         <div>
-                            <p><img src={planet.planet_icon} alt="planet image" width="75%"/></p>
+                            <p><img src={require(`../static/${planet.name}.png`).default} alt="planet image" width="75%"/></p>
                             <section id = "planet-image">
                                 <img src={require('../static/Left-Arrow.png').default} width="35px"  onClick={prevSlide}/>
-                                <img src={planet.image} alt="planet image" height="175px" width="175px"/>
+                                <img src={require(`../static/${planet.name}-Photo.jpeg`).default} alt="planet image" height="175px" width="175px"/>
                                 <img src={require('../static/Right-Arrow.png').default} width="35px" onClick={nextSlide}/>
                             </section>
                             <section id = "planet-info">
                             <p>{planet.name}</p>
                                 <p>Diameter: {planet.diameter} km</p>
                                 <p>Day Length: {planet['day-length']} Hours</p>
-                                <button>View {planet.name}</button>
+                                <button onCLick = {planet.big_image}>View {planet.name}</button>
                             </section>
                             <section id = "planet-data">
                                 <p>Discovered in {planet.discovered}</p>
@@ -45,6 +53,7 @@ const PlanetCarousel = ({ planets, slides }) => {
                         </div>)}
 
                    </div>
+                   </>
                )
            })}
         </section>
