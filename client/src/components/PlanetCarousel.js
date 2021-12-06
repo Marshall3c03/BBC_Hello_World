cccce62db2978a33a6243bc3da7fe8b688f9531d
Ross Napier <ross-napier@hotmail.com>
2021-12-06 20:09:45 +0000

    multiple slides added to carousel

--- client/src/components/PlanetCarousel.js
+++ client/src/components/PlanetCarousel.js
@@ -33,10 +33,26 @@ const PlanetCarousel = ({ planets, slides }) => {
                             
                         <div>
                             <p><img src={require(`../static/${planet.name}.png`).default} alt="planet image" width="75%"/></p>
+
+                            <section id = "planet-slide-block">
+
+                                    <div id = "duplicate-left">
+                                    <section id = "planet-image">
+                                        <img src={require(`../static/${planet.left_planet}-Photo.jpeg`).default} alt="planet image" height="175px" width="175px"/>
+                                    </section>
+                                    <section id = "planet-info">
+                                    <p>{planet.name}</p>
+                                        <p>Diameter: {planet.diameter} km</p>
+                                        <p>Day Length: {planet['day-length']} Hours</p>
+                                        <button onCLick = {planet.big_image}>View {planet.name}</button>
+                                    </section>
+                                    </div>
+
+                            <div id = "planet-main">
                             <section id = "planet-image">
-                                <img src={require('../static/Left-Arrow.png').default} width="35px"  onClick={prevSlide}/>
+                                <img class="arrow" src={require('../static/Left-Arrow.png').default} width="35px"  onClick={prevSlide}/>
                                 <img src={require(`../static/${planet.name}-Photo.jpeg`).default} alt="planet image" height="175px" width="175px"/>
-                                <img src={require('../static/Right-Arrow.png').default} width="35px" onClick={nextSlide}/>
+                                <img class="arrow" src={require('../static/Right-Arrow.png').default} width="35px" onClick={nextSlide}/>
                             </section>
                             <section id = "planet-info">
                             <p>{planet.name}</p>
@@ -44,6 +60,21 @@ const PlanetCarousel = ({ planets, slides }) => {
                                 <p>Day Length: {planet['day-length']} Hours</p>
                                 <button onCLick = {planet.big_image}>View {planet.name}</button>
                             </section>
+                            </div>
+
+                                <div id = "duplicate-right">
+                                    <section id = "planet-image">
+                                        <img src={require(`../static/${planet.right_planet}-Photo.jpeg`).default} alt="planet image" height="175px" width="175px"/>
+                                    </section>
+                                    <section id = "planet-info">
+                                    <p>{planet.name}</p>
+                                        <p>Diameter: {planet.diameter} km</p>
+                                        <p>Day Length: {planet['day-length']} Hours</p>
+                                        <button onCLick = {planet.big_image}>View {planet.name}</button>
+                                    </section>
+                                    </div>
+                            </section>
+
                             <section id = "planet-data">
                                 <p>Discovered in {planet.discovered}</p>
                                 <p>Surface Temperature {planet['surface-temperature']} &#8451;</p>
