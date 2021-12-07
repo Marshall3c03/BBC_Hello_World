b04260a35089e54f8c41f3518371733ca114dfc8
Craig Marshall <dev.craigmarshall@gmail.com>
2021-12-07 16:45:19 +0000

    finished styling About page

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -3,31 +3,39 @@ import './AboutPage.css'
 
 const AboutPage = () => {
     return(
-        <div>
+        <div className="theTeam">
+            <div className="brief-container">
+                <div className="row">
+                    <h2 >BBC Educational App</h2>
+                    <p className="brief-item">The BBC are looking to improve their online offering of educational content by developing some interactive browser applications that display information in a fun and interesting way. Your task is to make an a Minimum Viable Product or prototype to put forward to them - this may only be for a small set of information, and may only showcase some of the features to be included in the final app.</p>
+                </div>
+            </div>
             <h1>The Team</h1>
-            <div>
-                <p>
-                    Anita Safar
-                    <img id='avatar' src='https://preview.redd.it/c4tc8qfdnfl61.png?width=640&crop=smart&auto=webp&s=6cb2d2f033823939489bb79d670abfe6f3b27329' alt='person avatar'/>
-                </p>
-            </div>
-            <div>
-                <p>
-                    Craig Marshall
-                    <img id='avatar' src='https://img2.pngdownload.id/20180619/juy/kisspng-chimpanzee-ham-ape-bicycle-helmets-helmet-astronaut-5b29c19bd4dfd6.976971231529463195872.jpg' alt='person avatar'/>
-                </p>
-            </div>
-            <div>
-                <p>
-                    Ross Napier
-                    <img id='avatar' src='https://image.pngaaa.com/891/1886891-middle.png' alt='person avatar'/>
-                </p>
-            </div>
-            <div>
-                <p>
-                    Sam Spencer
-                    <img id='avatar' src='' alt='person avatar'/>
-                </p>
+            <div className="avatars">
+                <div className="person">
+                    <h3 className="names">
+                        Anita Safar
+                    </h3>
+                    <img id='avatar' src='https://cliply.co/wp-content/uploads/2020/10/442010812_HEADPHONES_AVATAR_3D_400px.gif' alt='person avatar' width="100px"/>
+                </div>
+                <div className="person">
+                    <h3 className="names">
+                        Craig Marshall
+                    </h3>
+                    <img id='avatar' src='https://cliply.co/wp-content/uploads/2020/09/442008571_ARTIST_AVATAR_3D_400px.gif' alt='person avatar'/>
+                </div>
+                <div className="person">
+                    <h3 className="names">
+                        Ross Napier
+                    </h3>
+                    <img id='avatar' src='https://cliply.co/wp-content/uploads/2020/09/442008572_ARTIST_AVATAR_3D_400px.gif' alt='person avatar'/>
+                </div>
+                <div className="person">
+                    <h3 className="names">
+                        Sam Spencer
+                    </h3>
+                    <img id='avatar' src='https://cliply.co/wp-content/uploads/2020/08/442008111_GLANCING_AVATAR_3D_400px.gif' alt='person avatar'/>
+                </div>
             </div>
         </div>
     );
