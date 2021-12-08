a10085b9ae19f58aef2c1c1c541ec76e1c516cab
Ross Napier <ross-napier@hotmail.com>
2021-12-08 13:16:58 +0000

    Planet pages added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,15 @@ import NavHeader from './components/NavHeader';
 import AboutPage from './components/AboutPage';
 import EasterEgg from './components/EasterEgg';
 import EarthPage from './components/EarthPage';
+import Jupiter from './components/Jupiter';
+import Uranus from './components/Uranus';
+import Pluto from './components/Pluto';
+import Saturn from './components/Saturn';
+import Mars from './components/Mars';
+import Mercury from './components/Mercury';
+import Sun from './components/Sun';
+import Venus from './components/Venus';
+import Neptune from './components/Neptune';
 import SplashPage from './components/SplashPage';
 import {Routes, Route} from 'react-router-dom'; 
 
@@ -64,7 +73,17 @@ function App() {
           <Route path='quiz' element={<PlanetQuiz />} />
           <Route path='about' element={<AboutPage />} />
           <Route path='easteregg' element={<EasterEgg />}/>
+
           <Route path='earth' element={<EarthPage />}/>
+          <Route path='jupiter' element={<Jupiter />}/>
+          <Route path='uranus' element={<Uranus />}/>
+          <Route path='pluto' element={<Pluto />}/>
+          <Route path='saturn' element={<Saturn />}/>
+          <Route path='mars' element={<Mars />}/>
+          <Route path='mercury' element={<Mercury />}/>
+          <Route path='sun' element={<Sun />}/>
+          <Route path='venus' element={<Venus />}/>
+          <Route path='neptune' element={<Neptune />}/>
 
         </Routes>
       </main>
---
+++ client/src/components/Uranus.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Uranus = () => {
+
+    return(
+        <img src = {require('../static/Uranus.jpeg').default} alt = "uranus" width = "100%" height = "100%"/>
+    );
+};
+
+export default Uranus;
