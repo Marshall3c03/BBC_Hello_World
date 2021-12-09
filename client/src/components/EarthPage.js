e36102ba70bd30b476389c235801589f2b4a0395
Sam <samspencer2202@outlook.com>
2021-12-09 07:02:09 +0000

    added launch sites, images still not displaying

--- client/src/components/EarthPage.js
+++ client/src/components/EarthPage.js
@@ -1,85 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactGlobe from 'react-globe';
+import { allMarkers } from './markers.js';
 
 //CSS ===============
 import '../static/CSS/EarthPage_style.css' ;
 //CSS ===============
 
-
 const EarthPage = () => {
 
-    const markers = [
+    const markers= [
         {
-          id: 'marker1',
-          city: 'Singapore',
-          info: 'info',
-          color: 'red',
-          coordinates: [1.3521, 103.8198],
-          value: 50,
+            id: 'marker1',
+            facility: 'Kennedy Space Center',
+            location: 'Cape Canaveral',
+            info: 'info',
+            color: 'rgba(224, 12, 9, 1)',
+            coordinates: [28.57427, -80.64907],
+            value: 50,
+            image: '../static/SpaceCenters/kennedy_space_center.jpeg'
         },
         {
-          id: 'marker2',
-          city: 'New York',
-          info: 'info',
-          color: 'blue',
-          coordinates: [40.73061, -73.935242],
-          value: 25,
+            id: 'marker2',
+            facility: 'Baikonur Cosmodrome',
+            location: 'Kazakhstan',
+            info: 'info',
+            color: 'rgba(252, 171, 16, 1)',
+            coordinates: [45.96480, 63.30523],
+            value: 25,
+            image: '../static/SpaceCenters/kazakhstan.jpg'
         },
         {
-          id: 'marker3',
-          city: 'San Francisco',
-          info: 'info',
-          color: 'orange',
-          coordinates: [37.773972, -122.431297],
-          value: 35,
+            id: 'marker3',
+            facility: 'Guiana Space Center',
+            location: 'Korurou (French Guiana)',
+            info: 'info',
+            color: 'rgba(60, 145, 230, 1)',
+            coordinates: [5.16756, -52.68326],
+            value: 35,
+            image: '../static/SpaceCenters/guiana.jpg'
         },
         {
-          id: 'marker4',
-          city: 'Beijing',
-          info: 'info',
-          color: 'gold',
-          coordinates: [39.9042, 116.4074],
-          value: 135,
+            id: 'marker4',
+            facility: 'Jiuquan Satellite Launch Center',
+            location: 'China',
+            info: 'info',
+            color: 'rgba(148, 75, 187,  1)',
+            coordinates: [40.98426, 100.20861],
+            value: 135,
+            image: '../static/SpaceCenters/china.jpg'
         },
         {
-          id: 'marker5',
-          city: 'London',
-          info: 'info',
-          color: 'green',
-          coordinates: [51.5074, 0.1278],
-          value: 80,
+            id: 'marker5',
+            facility: 'Satish Dhawan Space Center',
+            location: 'India',
+            info: 'info',
+            color: 'rgba(0, 168, 120, 1)',
+            coordinates: [13.72611, 80.22655],
+            value: 54,
+            image: '../static/SpaceCenters/india.jpg'
         },
         {
-          id: 'marker6',
-          city: 'Los Angeles',
-          info: 'info',
-          color: 'gold',
-          coordinates: [29.7604, -95.3698],
-          value: 54,
+            id: 'marker6',
+            facility: 'Rocket Lab Launch Complex One',
+            location: 'New Zealand',
+            info: 'info',
+            color: 'rgba(148, 75, 187,  1)',
+            coordinates: [-39.26027, 177.86624],
+            value: 54,
+            image: '../static/SpaceCenters/newzeland.jpeg'
         },
-      ];
+    ]
 
-      const options = {
-        cameraRotateSpeed: 0.5,
-        focusAnimationDuration: 2000,
+
+    // const [markers, setMarkers] = useState([]);
+    // const [event, setEvent] = useState(null);
+    // const [details, setDetails] = useState(null);
+
+    // const initMarkers = (mark) => {
+    //     const marks = allMarksImported[mark].markers,map(marker => ({
+    //         ...marker,
+    //         value: marker.value,
+    //     }))
+    //     return marks
+    // }
+    
+    // console.log(markers)
+    // const getTooltipContent = markers.map(({ marker }) => {
+    //     console.log(markers)
+    //     return(
+    //         <div>
+    //             <h2>{marker}</h2>
+    //             <p>{marker}</p>
+    //         </div>
+    //     )
+    // });
+
+    // const onClickMarker = (marker, markerObject, event) => {
+    //     setEvent({
+    //         type: 'CLICK',
+    //         marker,
+    //         markerObjectID: markerObject.uuid,
+    //         PointerEventPosition: { x: event.clientX, y: event.clientY },
+    //     });
+    //     setDetails(getTooltipContent(marker));
+    // }
+
+    const options = {
+        initialCoordinates: [28.57427, -80.64907],
+        markerType: 'bar',
+        cameraRotateSpeed: 1.5,
+        focusAnimationDuration: 1000,
         focusEasingFunction: ['Linear', 'None'],
-        // pointLightIntensity: 1.5,
         globeGlowColor: 'white',
-        markerTooltipRenderer: marker => `${marker.city} (${marker.info})`,
+        markerRadiusScaleRange: [0.3, 0.3],
+        markerTooltipRenderer: marker => { const tooltipContent = document.createElement("div");
+                                            tooltipContent.innerHTML = `<div style='background-color:black;' ><img src="${marker.image}" /> ${marker.facility} at (${marker.location})</div>`; 
+                                            return tooltipContent;
+                                        }
       };
-
     return (
         <>
             <div className='globe-container'>
-                <ReactGlobe className='globe' height="50vh" markers={markers} options={options}/>
+                <ReactGlobe 
+                    // onClickMarker={onClickMarker}
+                    className='globe' 
+                    height="75vh" 
+                    markers={markers} 
+                    options={options}
+                    globeBackgroundTexture={null}
+                />
             </div>
-            <div height="50vh" backgroundColor="red">
-                <p>
-                    Information about earth here
-                    -fact1
-                    -fact2
-                    -fact3
-                </p>
+            <div>
+                
             </div>
         </>
     );
